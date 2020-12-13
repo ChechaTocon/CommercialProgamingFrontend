5d6d1effceb1b191dfd4697827d3b17ce1698181
JuanjoORD <39684335+JuanjoORD@users.noreply.github.com>
2020-12-13 16:43:03 -0600

    listado de reviews y diseño para form crear editar

--- frontend/movies/src/app/components/review/list-review/list-review.component.ts
+++ frontend/movies/src/app/components/review/list-review/list-review.component.ts
@@ -1,4 +1,67 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs'
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { AddReviewComponent } from '../add-review/add-review.component'
+import { MatDialog } from "@angular/material/dialog";
+import { Review, User, Movie, CreateReview, Dialog } from '../../../models/review.model'
+
+const getReview = gql`
+  query {
+    reviews {
+      id
+      comment
+      ranking
+      createdAt
+      updatedAt
+      movie{
+        id
+        movieName
+      }
+      user{
+        id
+        username
+      }
+    }
+  }
+`;
+
+
+const createReview = gql`
+mutation createReview(
+  $comment: String,
+  $ranking: Int,
+  $movie: String,
+  $user: Int,
+) {
+
+  createReview(input: {
+    comment: $comment,
+    ranking: $ranking,
+    movie: $movie,
+    user: $user,
+  }) {
+    ok
+    review {
+      id
+      comment
+      ranking
+      createdAt
+      updatedAt
+      movie {
+        id
+        movieName
+      }
+      user{
+        id
+        username
+      }
+    }
+  }
+
+}
+`;
 
 @Component({
   selector: 'app-list-review',
@@ -7,14 +70,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListReviewComponent implements OnInit {
   public cargando:boolean = false
+  dataSource: Review[] = [new Review()]  
+  private querySubscription: Subscription | undefined;
 
-  constructor() { }
+  public baseDialog:Dialog | undefined
+
+  public userId:number | undefined
+
+  constructor(
+    private apollo: Apollo,
+    private _route:ActivatedRoute,
+    private _router:Router,    
+    public dialog: MatDialog
+  ) { }
 
   ngOnInit(): void {
+    this.querySubscription = this.apollo.watchQuery<any>({
+      query: getReview
+    })
+      .valueChanges
+      .subscribe(({ data }) => {
+        this.dataSource = data.reviews;
+        console.log(this.dataSource)
+        let datuser:any
+        datuser = localStorage.getItem("userLogged");
+        datuser = JSON.parse(datuser)
+        this.userId = datuser.id
+        console.log('data user:',  datuser)
+      });
   }
 
-  openDialog(id:number){
-    console.log('myid:',id)
-  }
+  newReview() {
+    this.apollo.mutate({
+      mutation: createReview,
+      variables: {
+        comment: 'Es entretenida solo la primera vez, pero no tanto como para verla otra vez',
+        ranking: 4,
+        movie: 'TW92aWVOb2RlOjE=',
+        user: this.userId,
+      }
+    }).subscribe(data => { 
+      let created:any
+      created = data
+      let mynewreview:Review
+      mynewreview = created.data.createReview.review
+      console.log('Se creo la review!', mynewreview);      
+      this.dataSource = [...this.dataSource, mynewreview] 
+    });
+  }  
 
 }
